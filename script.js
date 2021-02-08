fbc2862944e0363a6c980115140e34ff6f038200
Sarman7105 <sarman7105@gmail.com>
2021-02-08 22:18:11 +0600

    removing console log and improve search

--- script.js
+++ script.js
@@ -3,9 +3,10 @@ const searchBtn = document.getElementById('search-btn');
 searchBtn.addEventListener('click', function () {
     document.getElementById('error-container').style.display = "none";
     document.getElementById('foods').innerHTML = "";
+    document.getElementById('food-info').innerHTML = "";
     const searchForm = document.getElementById('search-foods');
     foodName = searchForm.value;
-    console.log('search string length', foodName.length);
+    
 
     //if the search string length is 1 then search by first letter else search by full name
     if (foodName.length > 1)
@@ -56,18 +57,15 @@ const creatingFoodsIfo = (foods) => {
 };
 
 function foodDivClick(){
-    console.log(this)
+    
     var childFoodName = this.children[1];
     foodName = childFoodName.innerText;
-    console.log({ foodName });
-    // console.log('children', childFoodName.innerText);
     var result = fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${foodName}`)
         .then(res => res.json())
         .then(data => {
             // const element=[];
             let name = "";
             let url = "";
-            console.log(data.meals.length);
             for (let i = 0; i < data.meals.length; i++){
                 if (foodName === data.meals[i].strMeal) {
                     name = data.meals[i].strMeal;
@@ -76,12 +74,10 @@ function foodDivClick(){
                 }
             }
             const element = data.meals[0];
-            // console.log('img src', element.strMealThumb);
-            // console.log('food name', element.strMeal);
             const foodDiv = document.createElement('div');
             const htmlTag = `<img src="${url}">
                             <h2>${name}</h2>
-                            <h4>Ingridients</h4>
+                            <h4>Ingredients</h4>
                             <ul>
                                 <li>${element.strIngredient1}</li>
                                 <li>${element.strIngredient2}</li>
